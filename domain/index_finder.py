a2286815593edc5ab6ac60e2599fab15dbebfa17
dbowers42 <dbowers.github@gmail.com>
2017-05-09 11:53:30 -0600

    convert tabs to spaces

--- domain/index_finder.py
+++ domain/index_finder.py
@@ -1,17 +1,17 @@
 def find_index(items):
-	buckets = {}
+    buckets = {}
 
-	for index, item in enumerate(items):
-		if not buckets.get(item):
-			buckets[item] = []
+    for index, item in enumerate(items):
+        if not buckets.get(item):
+            buckets[item] = []
 
-		buckets[item].append(index)
+        buckets[item].append(index)
 
-	filtered = [(key, buckets[key][0]) for key in buckets.keys() if len(buckets[key]) == 1]
+    filtered = [(key, buckets[key][0]) for key in buckets.keys() if len(buckets[key]) == 1]
 
-	result = sorted(filtered, key=lambda x: (x[1], x[1]))
+    result = sorted(filtered, key=lambda x: (x[1], x[1]))
 
-	return result if len(result) == 1 else None
+    return result if len(result) == 1 else None
 
 
 
--- domain/test_index_finder.py
+++ domain/test_index_finder.py
@@ -2,27 +2,27 @@ from domain.index_finder import find_index
 import unittest
 
 TEST_DATA_WITH_UNIQUE_ITEMS = [
-	(['A'], ('A', 0)),
-	(['A', 'B', 'C', 'A', 'C'], ('B', 1)),
-	(['A', 'A', 'B', 'B', 'C'], ('C', 4)),
-	(['A', 'B', 'A', ], ('B', 1)),
-	(['A', 'B', 'B', 'B'], ('A', 0)),
-	(['B', 'B', 'B', 'A'], ('A', 3))
+    (['A'], ('A', 0)),
+    (['A', 'B', 'C', 'A', 'C'], ('B', 1)),
+    (['A', 'A', 'B', 'B', 'C'], ('C', 4)),
+    (['A', 'B', 'A', ], ('B', 1)),
+    (['A', 'B', 'B', 'B'], ('A', 0)),
+    (['B', 'B', 'B', 'A'], ('A', 3))
 ]
 
 TEST_DATA_WITHOUT_UNIQUE_ITEM = ['A', 'B', 'C', 'C', 'B', 'A']
 
 
 class TestIndexFinder(unittest.TestCase):
-	def test_finds_first_unique_item(self):
-		for example in TEST_DATA_WITH_UNIQUE_ITEMS:
-			self.assertEqual(find_index(example[0]), example[1])
+    def test_finds_first_unique_item(self):
+        for example in TEST_DATA_WITH_UNIQUE_ITEMS:
+            self.assertEqual(find_index(example[0]), example[1])
 
-	def test_returns_none_for_empty_array(self):
-		self.assertEqual(find_index([]), None)
+    def test_returns_none_for_empty_array(self):
+        self.assertEqual(find_index([]), None)
 
-	def test_returns_none_for_no_unique_item(self):
-		self.assertEqual(TEST_DATA_WITHOUT_UNIQUE_ITEM, None)
+    def test_returns_none_for_no_unique_item(self):
+        self.assertEqual(TEST_DATA_WITHOUT_UNIQUE_ITEM, None)
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
